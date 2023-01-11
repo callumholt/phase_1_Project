e7c3e970a3cbcf31a0787192acb54de404dbaf39
callum <callumpholt@gmail.com>
2023-01-12 10:41:59 +1100

    cleaning up code

--- script.js
+++ script.js
@@ -1,40 +1,3 @@
-/* What needs to happen for this SPA?
-
-- need to have an input method on the SPA, aka a text box to submit a string of location.
-- need a button to submit the input.
-- on botton press (DOM event) the input needs to be added as a variable into the api as a GET request.
-- the GET request will come back with string of location data, temperature etc
-- need to convert the string into an object (.json() method) so that it we can iterate on it.
-- need to take specific pieces of data from the object and set to const.
-- need to use document.createelement to create a new element in the html which will 
-be use for the data from the const above.
-- need to take that const and push it to the dom using innerHTML.
-
-resources:
-https://rapidapi.com/weatherapi/api/weatherapi-com/
-
-
---------------
-Use an API
-
-Your entire app must run on a single page. 
-
-Use at least 3 unique event-listeners Links to an external site. that enable interactivity. 
-Think search or filter functionality, toggling dark/light mode, upvoting posts, etc. 
-Each of your event listeners should have its own unique callback function.
-
-Your project must implement at least one instance of array iteration using available 
-array methods (map, forEach, filter, etc). 
-
-
-Manipulating your API data in some way should present an opportunity to implement your array iteration.
-
-Follow good coding practices. Keep your code DRY (Do not repeat yourself) by utilizing functions to abstract repetitive code.
-
-
-*/
-
-
 function userinput() {
 
 	let input = document.getElementById("cityLocation").value;
@@ -42,8 +5,6 @@ function userinput() {
 	
 
 }
-
-// const fetchData;
 
 function returnText()
 {
@@ -60,29 +21,19 @@ function returnText()
 	 	.then(response => response.json())
 
 		.then((data) =>  {
-			console.log(data);
 
-			//setting current Temp
 			currentTemp = data.current.temp_c;
 			let para = document.getElementById("currentTempPara");
 			let h2 = document.getElementById("temp");
 			h2.innerHTML = currentTemp;
-			console.log(currentTemp)
 
-
-		//setting cityName and countryName
-
-			//cityName
 			let nameOfCity = data.location.name;
 			let cityName = document.getElementById("cityName");
 			cityName.innerHTML = nameOfCity;
-			console.log(nameOfCity)
 			
-			//countryName
 			let nameOfCountry = data.location.country;
 			let countryName = document.getElementById("countryName");
 			countryName.innerHTML = nameOfCountry;
-			console.log(countryName)
 
 			showHideTemp();
 
@@ -93,7 +44,6 @@ function returnText()
 	 	.catch(err => console.error(err));
 	}
 	
-		//show/hide temp text
 
 	function showHideTemp() {
 		let currentTempElement = document.getElementById("currentTemp");
@@ -110,7 +60,6 @@ function returnText()
 		time.appendChild(Button);
 	}
 	
-	//variables for mouseOverFunction
 	
 
 	function mouseOverFunction() {
@@ -139,26 +88,16 @@ function returnText()
 				timeButton.appendChild(cityTime);
 				cityTime.innerHTML = time;
 
-				//show save button
-
 				let saveButtonDiv = document.getElementById("save")
 				saveButtonDiv.style.display = ""; 
 			}
-			
-			console.log(time);
-					
+								
 		})
 	}
 
-//event listener, listening to the click of button id="save"
 let saveButton = document.getElementById("saveButton")
 saveButton.addEventListener('click',saveToServer);
-
-//console.log("global", input) //pointer event...?
-//console.log("global", currentTemp) //undefined
-//console.log("global", time) //undefined
-
-// save the data to the server function	
+	
 function saveToServer() {
 
 	let methodForPostRequest =
@@ -174,53 +113,17 @@ function saveToServer() {
 			localTime: `${time}`,
 		})
 
-		//just for storing if i need to reverse changes.
-	
-
-		/** 
-		 body: JSON.stringify({
-			id: "",
-			city: "Darwin",
-			Temp: "14",
-			localTime: "9:00",
-		})
-		 */
-
 	  }
 
 	  fetch("http://localhost:3000/weather", methodForPostRequest)
 	  .then((resp)=> resp.json())
-	  .then((data)=>
-	  
-	  {
+	  .then((data)=> {
 		if(data) {
 			alert("Information was successfully saved to your account");
 		}
 	  })
+	  .catch(err => alert("was unable to reach server"));
+
 }
 
 		
-
-
-
-
-
-
-/*
-// Get a reference to the <button> element
-var button = document.getElementById('myButton');
-
-// Create the event listener function
-function handleClick(event) {
-  // Do something when the event occurs
-}
-
-// Attach the event listener to the <button> element
-button.addEventListener('click', handleClick);
-
-*/
-
-
-
-
-
